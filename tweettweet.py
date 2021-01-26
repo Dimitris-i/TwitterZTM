7add1f15f8e5acb8e284946da8889ad86acb8f30
Dimitri <diliou.gr@gmail.com>
2021-01-25 20:27:00 -0500

    TwitterZTMProject

---
+++ tweettweet.py
@@ -0,0 +1,51 @@
+import tweepy
+import time
+
+def print_public_tweets(api):
+    public_tweets = api.home_timeline()
+    for tweet in public_tweets:
+        print(tweet.text)
+
+def limit_handler(cursor):
+    try:
+        while True:
+            yield cursor.next()
+    except tweepy.RateLimitError:
+        time.sleep(1000)
+    except StopIteration:
+        print('That\'s all folks')
+
+def follow_user(username):
+    '''
+    We wrap the Cursor with the limit_handler to pause when we hit the Twitter
+    limit of pinging the server
+    '''
+    for follower in limit_handler(tweepy.Cursor(api.followers).items()):
+        if follower.name == username:
+            follower.follow()
+            break
+
+def like_numOfTweets_with_keyword(search_keyword, numberOfTweets):
+    for tweet in limit_handler(tweepy.Cursor(api.search, search_keyword).items(numberOfTweets)):
+        try 
+            tweet.favorite()
+            print('I liked that tweet')
+        except tweepy.TweepError as e:
+            print(e.reason)
+        except StopIteration:
+            break
+
+auth = tweepy.OAuthHandler('l3v7Bzgl9I6Z6oXmlBGAHf30b','XbSgzch7N0cf0t9G0V0mXYBJzNcO2xUhwhsVPjtvOFurct78vs')
+auth.set_access_token('318289151-E1ed2mxTgy0yaoHYOxUXzHJfXVIUvrjxUMThYeJ1', 'IwWxTf9spQxaOQ9sWtTeRofF4kFHMPbuXPdMb5KleYTgl')
+
+api = tweepy.API(auth)
+user = api.me()
+username = 'ElonMusk'
+
+print_public_tweets(api)
+follow_user('username')
+
+search_string = 'python'
+numberOfTweets = 2
+
+like_numOfTweets_with_keyword(search_string, numberOfTweets)
